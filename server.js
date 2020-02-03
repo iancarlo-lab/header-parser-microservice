72ba2521ada97f7edee5e2f3eba1f5a91b82c75d
Adrian <murilloadrianc@gmail.com>
2020-02-03 13:24:25 -0300

    added json

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
 const express = require('express');
 const path = require('path');
+var os = require( 'os' );
+
+var networkInterfaces = os.networkInterfaces( );
+
 const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname , 'client/build' )));
 
-const jsObj = {
-    ipadress: "",
-    language: "",
-    software: "",
-    ip: ""
-}
-
-app.get('/', (req,res) => {
-    res.send(req.headers.host)
-})
 
 app.get('/api/whoiam', (req, res) => {
     const headers = JSON.stringify(req.headers["accept-language"]);
     const ips = JSON.stringify(req.headers.host)
     const soft = JSON.stringify(req.headers["user-agent"]);
-    const Ip = (req.ipInfo) 
-    console.log(req.ipInfo)
+    const Ip = JSON.stringify(req.headers['x-forwarded-for']);
 
-     jsObj.ipadress = `${ips}`;
-     jsObj.language = `${headers}`;
-     jsObj.software = `${soft}`;
-     jsObj.ip = `${Ip}`
 
+    const jsObj = [{
+        ipadress: ips,
+        language: headers,
+        software: soft,
+        ip: Ip
+    }]
+    console.log(networkInterfaces)
+    //console.log(jsObj)
     res.json(jsObj)
 });
 
